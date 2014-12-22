443ad65db0a3ee96ca0724f0d7a921066630d15d
Liwen Sun <sunliwen@gmail.com>
2014-12-22 13:10:12 +0800

    Update versioning for py3 fork

--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ def find_packages_by_root_package(where):
 # -----------------------------------------------------------------------------
 setup(
     name="behave-py3",
-    version="1.2.5a1",
+    version="1.2.5a1_2014122201",
     description="""behave is behaviour-driven development, Python style.
                    behave-py3 is a personal fork with python 3 support.
                    Hope it could be merged back to upstream.""",
